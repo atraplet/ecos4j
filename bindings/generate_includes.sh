f858d34743cabcceec511c4e6d7ed8b4a86b5394
Adrian Trapletti <a.trapletti@ustermetrics.com>
2024-06-09 18:45:39 +0200

    Exclude SuiteSparse

--- bindings/generate_includes.sh
+++ bindings/generate_includes.sh
@@ -26,6 +26,6 @@ ${JEXTRACT} \
   --dump-includes "${TMP_INCLUDES}" \
   "${ECOS}"/include/ecos.h
 
-# select ecos symbols plus fflush() for flushing C buffers, exclude timer
-grep "ecos\|fflush" "${TMP_INCLUDES}" | grep -v "timer\|fflush_nolock\|fflush_unlocked" >"${INCLUDES}"
+# select symbols
+grep "ecos\|fflush" "${TMP_INCLUDES}" | grep -v "SuiteSparse\|timer\|fflush_nolock\|fflush_unlocked" >"${INCLUDES}"
 rm -f "${TMP_INCLUDES}"
